96f8a09bd9c52737762891fe08d6bc39fbfc14ab
MantaYuana <gunamanta.yuana@gmail.com>
2024-12-03 22:36:06 +0800

    implemented bidang kemahasiswaan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,31 @@
 variants: {
-    display: ['group-hover']
+  display: ["group-hover"];
 }
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-        extend: {
-            screens: {
-                print: { raw: 'print' },
-                screen: { raw: 'screen' },
-            },
-            colors: {
-                'primary': '#aaaaaa',
-                'secondary': '#3f3cbb',
-                'accent': '#121063',
-                'dark': '#112D4E',
-                'light': '#3ab7bf',
-                'ti-dark-brown': '#785233',
-                'ti-yellow-brown': '#ECBB4E',
-            },
-            fontFamily: {
-                'work-sans': ['"Work Sans"', 'sans-serif'],
-            },
-        }
-    }
-}
+  theme: {
+    container: {
+      padding: "2rem",
+    },
+    extend: {
+      screens: {
+        print: { raw: "print" },
+        screen: { raw: "screen" },
+      },
+      colors: {
+        primary: "#aaaaaa",
+        secondary: "#3f3cbb",
+        accent: "#121063",
+        dark: "#112D4E",
+        light: "#3ab7bf",
+        "ti-dark-brown": "#785233",
+        "ti-yellow-brown": "#ECBB4E",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+        // 'work-sans': ['"Work Sans"', 'sans-serif'],
+      },
+    },
+  },
+};
